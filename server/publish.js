824d18b95252353d04958abe79941a06238b8494
maxc <maxc@cnliren.cn>
2017-11-27 10:14:41 +0800

    修复publish文件

--- server/publish.js
+++ server/publish.js
@@ -25,6 +25,22 @@ Meteor.startup(function () {
     Meteor.publish('xitongguanli_mabiaoxx', function () {
         return ts_gc_mabiaoxx.find({});
     });
+    Meteor.publish('zijinbofu_gongdanxx', function () {
+        return tb_gc_zijinbflcxx.find({}/*,{fields:{
+            'shenpidbh':true,
+            'shoukuandw':true,
+            'fuwulxbh':true,
+            'fuwulxmc':true,
+            'xiangmubh':true,
+            'xiangmumc':true,
+            'gongchengxz':true,
+            'hetongje':true,
+            'hetongqdj':true,
+            'zhishangqljbfzj':true,
+            'dangqiangdzt':true,
+            'dangqianclzt':true,
+        }}*/);
+    });
     Meteor.publish('zijinbofu_xiangmuk', function () {
         return tb_gc_xiangmuk.find({}/*,{fields:{
             'shenpidbh':true,
